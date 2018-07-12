97729d5f841910ca393a969d632027eb943bdef3
Adil Ansari <ansariadil81@gmail.com>
2018-07-12 14:14:22 +0530

    setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(`mongodb://ansariadil81@gmail.com:@9013146767aD@ds233571.mlab.com:33571/adil1995` || 'mongodb://localhost:27017/TodoApp');
 
 module.exports ={mongoose};
