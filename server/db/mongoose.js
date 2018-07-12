f727cf149c3752a3886ad17ba19431f36d82a238
Adil Ansari <ansariadil81@gmail.com>
2018-07-12 15:06:35 +0530

    non

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ let mongoose = require('mongoose')
 let MONGOLAB_URI="mongodb://Adil1995:1234567890a@ds233571.mlab.com:33571/adil1995"
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports ={mongoose};
