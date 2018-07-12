cc806e35a1630b95f8b39b429cf31528e8710191
Adil Ansari <ansariadil81@gmail.com>
2018-07-12 15:41:36 +0530

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,4 @@
 let mongoose = require('mongoose')
-let MONGOLAB_URI="mongodb://Adil1995:1234567890a@ds233571.mlab.com:33571/adil1995"
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
